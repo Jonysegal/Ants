766719c3892ad5d30bd46f226c597d1879085bb2
Jonysegal <jonysegal@gmail.com>
2020-06-19 10:04:30 -0400

    Add files via upload

---
+++ MainControl.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+
+namespace deee
+{
+    class MainControl
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Worldff!");
+            Initialize();
+            RunLoop();
+            
+        }
+
+        static void Initialize()
+        {
+            WorldController.Initialize();
+        }
+
+        static void RunLoop()
+        {
+            while (true)
+            {
+                WorldController.Loop();
+                Drawer.Loop();
+            }
+        }
+    }
+}
